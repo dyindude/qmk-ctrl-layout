00942ae84797bfe00cbf625bf80050fafb2e4712
dyindude <dyindude@github.dyindude.net>
2019-05-07 10:26:20 -0500

    change light modes to use _noeeprom versions (I'm assuming so we don't write too much to eeprom..)

--- keymap.c
+++ keymap.c
@@ -16,8 +16,8 @@ uint32_t default_mode = 22;
 
 //default 
 void matrix_init_user() {
-    rgblight_mode(default_mode);
-    rgb_matrix_sethsv(HSV_MAGENTA);
+    rgblight_mode_noeeprom(default_mode);
+    rgb_matrix_sethsv_noeeprom(HSV_MAGENTA);
 }
 
 //this file provides keymaps, so we can do more complex stuff here
@@ -26,12 +26,12 @@ void matrix_init_user() {
 uint32_t layer_state_set_user(uint32_t state) {
     switch (biton32(state)) {
         case _META:
-            rgblight_mode(3);
-            rgb_matrix_sethsv(HSV_MAGENTA);
+            rgblight_mode_noeeprom(3);
+            rgb_matrix_sethsv_noeeprom(HSV_MAGENTA);
             break;
         default: //  for any other layers, or the default layer
-            rgblight_mode(default_mode);
-            rgb_matrix_sethsv(HSV_MAGENTA);
+            rgblight_mode_noeeprom(default_mode);
+            rgb_matrix_sethsv_noeeprom(HSV_MAGENTA);
             break;
     }
     return state;
