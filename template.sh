c035bec7730867d936272be20f327c266365c181
dyindude <dyindude@users.noreply.github.com>
2019-05-08 15:25:45 -0500

    Create template.sh
    hacky, but works? ish

---
+++ template.sh
@@ -0,0 +1,8 @@
+length=$(($(cat keymap.json | jq -cM ".layers | length")-1))
+echo '//layouts..using QMK configurator to generate these for now
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {'
+for i in $(seq 0 $length);do
+    echo -n "[$i] = LAYOUT("
+    cat keymap.json | jq -cM ".layers[$i]" | sed 's/\"//g;s/\[//g;s/\]/)/g'
+done
+echo '};'
