a801d09cee61801f6219f8f4e9d1b495b42ffa6e
dyindude <dyindude@github.dyindude.net>
2019-05-10 18:59:40 -0500

    also selecting random hsv value when returning to base layer

--- keymap.c
+++ keymap.c
@@ -52,10 +52,8 @@ int rand_min_max(int min, int max) {
     return rand() % (max + 1 - min) + min;
 }
 
-int select_random_layout(void) {
-    int swatch;
-    swatch = rand_min_max(0, 1);
-    if (swatch) {
+int select_random_mode(void) {
+    if (rand_min_max(0, 1)) {
         return rand_min_max(21,24);
     } else {
         return rand_min_max(2,5);
@@ -81,8 +79,12 @@ uint32_t layer_state_set_user(uint32_t state) {
         default: //  for any other layers, or the default layer
             //doesn't work..may need MASSDROP_CONFIGURATOR to do this
             //rgb_matrix_set_color(0, 0xFF, 0x00, 0xFF);
-            rgblight_mode_noeeprom(select_random_layout()); //example selecting random mode
-            rgb_matrix_sethsv_noeeprom(HSV_MAGENTA);
+            rgblight_mode_noeeprom(select_random_mode()); //example selecting random mode
+            if (rand_min_max(0,1)){
+                rgb_matrix_sethsv_noeeprom(HSV_MAGENTA);
+            } else {
+                rgb_matrix_sethsv_noeeprom(HSV_PINK);
+            }
             break;
     }
     return state;
